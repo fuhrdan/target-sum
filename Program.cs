1b3f836574fb7fd9ce8fccc168bf44fb3cc9d5dc
fuhrdan <165685619+fuhrdan@users.noreply.github.com>
2024-12-26 09:37:02 -0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿//*****************************************************************************
+//** 494. Target Sum                                                leetcode **
+//*****************************************************************************
+
+int findTotalWays(int* nums, int numsSize, int i, int s, int target)
+{
+    if (s == target && i == numsSize)
+    {
+        return 1;
+    }
+
+    if (i >= numsSize)
+    {
+        return 0;
+    }
+
+    return findTotalWays(nums, numsSize, i + 1, s + nums[i], target) +
+           findTotalWays(nums, numsSize, i + 1, s - nums[i], target);
+}
+
+int findTargetSumWays(int* nums, int numsSize, int target)
+{
+    return findTotalWays(nums, numsSize, 0, 0, target);
+}
